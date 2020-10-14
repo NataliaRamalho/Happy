b45e27031c55fa5f63016496a9661f045f6cef1c
Natália Ramalho <NataliaRamalho@users.noreply.github.com>
2020-10-14 17:03:05 -0300

    Dia 1 criação do projeto e paginas iniciais(frontend)

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import mapMarkerImg from '../images/map-marker.svg'
+
+import {Link} from 'react-router-dom';
+
+import {FiPlus} from 'react-icons/fi';
+
+import {Map, TileLayer} from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import '../styles/pages/orphanages-map.css'
+
+function OrphanagesMap() {
+  return(
+    <div id="page-map">
+      <aside>
+          <header>
+             <img src={mapMarkerImg} alt="Happy"/>
+             <h2> Escolha um orfanato no mapa</h2>
+             <p>Muitas crianças estão esperando a sua visita</p>
+          </header>
+        
+         <footer>
+           <strong> Londrina </strong>
+           <span> Paraná</span>
+         </footer>
+       </aside>
+       <Map 
+         center = {[-23.321264,-51.2358034]}
+         zoom={15}
+         style= {{width:'100%',height: '100%'}}
+       > 
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
+         
+       </Map>
+       <Link to="/" className="create-orphanage">
+          <FiPlus size ={32} color='#FFF'></FiPlus> 
+       </Link>      
+    </div>
+   )
+}
+
+export default OrphanagesMap;
